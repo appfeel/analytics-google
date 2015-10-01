263cda29832947a4395e04339e0093905cc213fd
Miquel Martín <mmgadmin@MacMiquel.local>
2015-10-02 01:16:09 +0200

    Update hooks

--- hooks/after_prepare/999-admobPluginNotice.js
+++ hooks/after_prepare/999-admobPluginNotice.js
@@ -3,21 +3,20 @@
 // Show info about AdMobAds Plugin
 
 module.exports = function (context) {
-  var plugin = context.requireCordovaModule('cordova-lib/src/cordova/plugin'),
-    showLegend = true,
-    pluginNames = context.opts.cordova.plugins;
+    var showLegend = true,
+        pluginNames = context.opts.cordova.plugins;
 
-  pluginNames.forEach(function (pluginName) {
-    if (~pluginNames.indexOf('cordova-admob') || ~pluginNames.indexOf('cordova-admob-ads')) {
-      showLegend = false;
+    pluginNames.forEach(function (pluginName) {
+        if (pluginName.indexOf('admob') < 0) {
+            showLegend = false;
+        }
+    });
+
+    if (showLegend) {
+        console.log();
+        console.log('*********** Monetize your app with AdMob ads. Now available with this cordova / phonegap plugin:');
+        console.log('*********** cordova plugin add cordova-admob');
+        console.log('*********** Docs: https://github.com/appfeel/admob-google-cordova');
+        console.log();
     }
-  });
-
-  if (showLegend) {
-    console.log();
-    console.log('*********** Monetize your app with AdMob ads. Now available with this cordova / phonegap plugin:');
-    console.log('*********** cordova plugin add cordova-admob');
-    console.log('*********** Docs: https://github.com/appfeel/admob-google-cordova');
-    console.log();
-  }
 };
